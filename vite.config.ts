9caf200b2857408ce9d8dfdc7978b6a5431e716a
KukoCL <webphantom.hc@gmail.com>
2025-01-14 21:51:24 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/legal-studio-app/',
   plugins: [
     vue(),
     vueDevTools(),
