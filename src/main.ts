2e176bea018a0525934b350d9ce914e46325d2d3
KukoCL <webphantom.hc@gmail.com>
2025-01-14 22:14:26 -0300

    router

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,35 @@ import "bootstrap";
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
-import router from '@/router/router';
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router';
+import HomePage from './pages/HomePage.vue';
+import FeaturesPage from './pages/FeaturesPage.vue';
+
+// Define your routes
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/', // This redirects to the default route
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+    component: HomePage,
+  },
+  {
+    path: '/features',
+    component: FeaturesPage,
+  },
+  {
+    path: '/:pathMatch(.*)*', // This is a catch-all route
+    redirect: '/home',
+  },
+];
+
+// Create the router instance with createWebHashHistory
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
 
 const app = createApp(App);
 
